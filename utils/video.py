8a358b7cb50b4da3901314f7ee26b9d535abfa39
Harish Kumar <harishghp@gmail.com>
2023-07-27 15:33:36 +0530

    Use only the first frame of the frame stack

--- utils/video.py
+++ utils/video.py
@@ -1,12 +1,15 @@
 import cv2
 from utils.segment import Segment
+import os
+import numpy as np
 
 def generate_video_from_grayscale_doom_play_segments(segment: Segment, output_path: str, fps: int = 30):
-    print("Generating video from segment...")
+    output_path = os.path.abspath(output_path)
     num_steps, num_channels, frame_height, frame_width = segment.observations.shape
-    print(f"segment.observations.shape={segment.observations.shape}")
-    frames = segment.observations.reshape(num_steps * num_channels, frame_height, frame_width)
-    print(f"frames.shape={frames.shape}")
+
+    frames = np.zeros((num_steps, frame_height, frame_width), dtype=segment.observations.dtype)
+    for i in range(num_steps):
+        frames[i] = segment.observations[i][0]
 
     # Define the codec and create a video writer
     fourcc = cv2.VideoWriter_fourcc(*'H264')  # Codec for MP4 file format
@@ -14,14 +17,11 @@ def generate_video_from_grayscale_doom_play_segments(segment: Segment, output_pa
 
     i = 0
     for frame in frames:
-        print(f"Writing frame {i}...")
         # Convert the grayscale frame to BGR format
         bgr_frame = cv2.cvtColor(frame, cv2.COLOR_GRAY2BGR)
         # Write the BGR frame to the output video
         output.write(bgr_frame)
-        print(f"Successfully wrote frame {i}!")
         i += 1
 
     # Release video sources and writer
     output.release()
-    print("Generated video from segment!")
