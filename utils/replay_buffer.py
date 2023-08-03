cd6789c04a5c7c78ff4ff61eda32caa4df0552d5
Harish Kumar <harishghp@gmail.com>
2023-08-03 22:31:53 +0530

    Fix new segmenting function in replay buffer

--- utils/replay_buffer.py
+++ utils/replay_buffer.py
@@ -133,30 +133,28 @@ class ReplayBuffer:
     
     def get_segments(self, segment_length: int):
         segments = []
+        
+        assert self.num_steps % segment_length == 0, "The segment length must be a divisible of the number of steps in the segment"
 
         # Looping through all the steps, one environment at a time
         for env in range(self.num_envs):
-            current_segment_start_step = 0
-            for step in range(self.num_steps):
-                num_steps_in_segment = step - current_segment_start_step
-                if step % segment_length == 0:
-                    # Creating new segment
-                    segment = Segment(num_steps_in_segment, 
-                                      self.raw_observation_space, 
-                                      self.processed_observation_space, 
-                                      self.action_space)
-                    for buffer_step in range(current_segment_start_step, step):
-                        segment_step = current_segment_start_step % segment_length
-                        segment[segment_step] = (self.raw_observations[buffer_step][env], 
-                                                 self.processed_observations[buffer_step][env],
-                                                 self.actions[buffer_step][env],
-                                                 self.log_probs[buffer_step][env],
-                                                 self.rewards[buffer_step][env],
-                                                 self.values[buffer_step][env],
-                                                 self.dones[buffer_step][env])
-                    segments.append(segment)
-
-                    # Updating current segment step
-                    current_segment_start_step = step
+            # Creating segments of the provided size
+            for step in range(0, self.num_steps, segment_length):
+                current_segment_start_step = step
+                segment = Segment(segment_length, 
+                                    self.raw_observation_space, 
+                                    self.processed_observation_space, 
+                                    self.action_space)
+                segment_step = 0
+                for buffer_step in range(current_segment_start_step, current_segment_start_step + segment_length):
+                    segment[segment_step] = (self.raw_observations[buffer_step][env], 
+                                                self.processed_observations[buffer_step][env],
+                                                self.actions[buffer_step][env],
+                                                self.log_probs[buffer_step][env],
+                                                self.rewards[buffer_step][env],
+                                                self.values[buffer_step][env],
+                                                self.dones[buffer_step][env])
+                    segment_step += 1
+                segments.append(segment)
 
         return segments
